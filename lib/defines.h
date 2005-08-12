c911b9c5b20b02367fec6c51eac49c0b4c52fd94
Simon Josefsson <simon@josefsson.org>
2005-08-12 13:20:27 +0000

    Fix error.

--- lib/defines.h
+++ lib/defines.h
@@ -40,38 +40,36 @@
 # include <sys/types.h>
 #endif
 
-
 #if HAVE_INTTYPES_H
 # include <inttypes.h>
 #else
 # if HAVE_STDINT_H
 #  include <stdint.h>
 # else
-#if SIZEOF_UNSIGNED_LONG_INT == 4
+#  if SIZEOF_UNSIGNED_LONG_INT == 4
 typedef unsigned long int uint32;
 typedef signed long int sint32;
-#elif SIZEOF_UNSIGNED_INT == 4
+#  elif SIZEOF_UNSIGNED_INT == 4
 typedef unsigned int uint32;
 typedef signed int sint32;
-#else
-# error "Cannot find a 32 bit integer in your system, sorry."
-#endif
-
-#if SIZEOF_UNSIGNED_INT == 2
+#  else
+#   error "Cannot find a 32 bit integer in your system, sorry."
+#  endif
+#  if SIZEOF_UNSIGNED_INT == 2
 typedef unsigned int uint16;
 typedef signed int sint16;
-#elif SIZEOF_UNSIGNED_SHORT_INT == 2
+#  elif SIZEOF_UNSIGNED_SHORT_INT == 2
 typedef unsigned short int uint16;
 typedef signed short int sint16;
-#else
-# error "Cannot find a 16 bit integer in your system, sorry."
-#endif
-
-#if SIZEOF_UNSIGNED_CHAR == 1
+#  else
+#   error "Cannot find a 16 bit integer in your system, sorry."
+#  endif
+#  if SIZEOF_UNSIGNED_CHAR == 1
 typedef unsigned char uint8;
 typedef signed char int8;
-#else
-# error "Cannot find an 8 bit char in your system, sorry."
+#  else
+#   error "Cannot find an 8 bit char in your system, sorry."
+# endif
 #endif
 
 #endif	/* defines_h */
