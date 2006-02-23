b054d0ff14283892e959992381c71ea17890c3d5
Simon Josefsson <simon@josefsson.org>
2006-02-23 12:21:50 +0000

    Typo.

--- lib/libtasn1.h
+++ lib/libtasn1.h
@@ -198,8 +198,8 @@ void libtasn1_perror(asn1_retCode error);
   int asn1_get_tag_der(const unsigned char *der, int der_len,
 		       unsigned char *class,int  *len, unsigned long *tag);
 
-  void _asn1_octet_der(const unsigned char *str,int str_len,
-		       unsigned char *der,int *der_len);
+  void asn1_octet_der(const unsigned char *str,int str_len,
+		      unsigned char *der,int *der_len);
 
   asn1_retCode asn1_get_octet_der(const unsigned char *der, int der_len,
 				  int *ret_len,unsigned char *str,
