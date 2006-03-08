4657f74fab2f383848b66ba91d72ba5cbcdab8ee
Simon Josefsson <simon@josefsson.org>
2006-03-08 14:32:24 +0000

    Map back TYPE_* and CLASS_*.

--- lib/int.h
+++ lib/int.h
@@ -48,7 +48,6 @@
 /****************************************/
 #define type_field(x)     (x&0xFF)
 
-/* List of constants for field type of typedef node_asn  */
 #define TYPE_CONSTANT ASN1_TYPE_CONSTANT
 #define TYPE_IDENTIFIER ASN1_TYPE_IDENTIFIER
 #define TYPE_INTEGER ASN1_TYPE_INTEGER
@@ -72,41 +71,28 @@
 #define TYPE_ENUMERATED ASN1_TYPE_ENUMERATED
 #define TYPE_GENERALSTRING ASN1_TYPE_GENERALSTRING
 
-/***********************************************************************/
-/* List of constants to better specify the type of typedef node_asn.   */
-/***********************************************************************/
-/*  Used with TYPE_TAG  */
-#define CONST_UNIVERSAL   (1<<8)
-#define CONST_PRIVATE     (1<<9)
-#define CONST_APPLICATION (1<<10)
-#define CONST_EXPLICIT    (1<<11)
-#define CONST_IMPLICIT    (1<<12)
-
-#define CONST_TAG         (1<<13)  /*  Used in ASN.1 assignement  */
-#define CONST_OPTION      (1<<14)
-#define CONST_DEFAULT     (1<<15)
-#define CONST_TRUE        (1<<16)
-#define CONST_FALSE       (1<<17)
-
-#define CONST_LIST        (1<<18)  /*  Used with TYPE_INTEGER and TYPE_BIT_STRING  */
-#define CONST_MIN_MAX     (1<<19)
-
-#define CONST_1_PARAM     (1<<20)
-
-#define CONST_SIZE        (1<<21)
-
-#define CONST_DEFINED_BY  (1<<22)
-
-#define CONST_GENERALIZED (1<<23)
-#define CONST_UTC         (1<<24)
-
-/* #define CONST_IMPORTS     (1<<25) */
-
-#define CONST_NOT_USED    (1<<26)
-#define CONST_SET         (1<<27)
-#define CONST_ASSIGN      (1<<28)
-
-#define CONST_DOWN        (1<<29)
-#define CONST_RIGHT       (1<<30)
+#define CONST_UNIVERSAL ASN1_CONST_UNIVERSAL
+#define CONST_PRIVATE ASN1_CONST_PRIVATE
+#define CONST_APPLICATION ASN1_CONST_APPLICATION
+#define CONST_EXPLICIT ASN1_CONST_EXPLICIT
+#define CONST_IMPLICIT ASN1_CONST_IMPLICIT
+#define CONST_TAG ASN1_CONST_TAG
+#define CONST_OPTION ASN1_CONST_OPTION
+#define CONST_DEFAULT ASN1_CONST_DEFAULT
+#define CONST_TRUE ASN1_CONST_TRUE
+#define CONST_FALSE ASN1_CONST_FALSE
+#define CONST_LIST ASN1_CONST_LIST
+#define CONST_MIN_MAX ASN1_CONST_MIN_MAX
+#define CONST_1_PARAM ASN1_CONST_1_PARAM
+#define CONST_SIZE ASN1_CONST_SIZE
+#define CONST_DEFINED_BY ASN1_CONST_DEFINED_BY
+#define CONST_GENERALIZED ASN1_CONST_GENERALIZED
+#define CONST_UTC ASN1_CONST_UTC
+/* #define CONST_IMPORTS ASN1_CONST_IMPORTS */
+#define CONST_NOT_USED ASN1_CONST_NOT_USED
+#define CONST_SET ASN1_CONST_SET
+#define CONST_ASSIGN ASN1_CONST_ASSIGN
+#define CONST_DOWN ASN1_CONST_DOWN
+#define CONST_RIGHT ASN1_CONST_RIGHT
 
 #endif /* INT_H */
