f191c4c88422d008ad624206fa065719070ff8b2
Simon Josefsson <simon@josefsson.org>
2008-08-29 12:48:32 +0200

    Fix release target.

--- cfg.mk
+++ cfg.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2006, 2007 Free Software Foundation
+# Copyright (C) 2006, 2007, 2008 Free Software Foundation
 # Author: Simon Josefsson
 #
 # This file is part of LIBTASN1.
@@ -32,3 +32,32 @@ W32ROOT ?= $(HOME)/w32root
 
 mingw32: autoreconf 
 	./configure --enable-gtk-doc --host=i586-mingw32msvc --build=`./config.guess` --prefix=$(W32ROOT)
+
+ChangeLog:
+	git2cl > ChangeLog
+	cat .clcopying >> ChangeLog
+
+htmldir = ../www-$(PACKAGE)
+tag = $(PACKAGE)_`echo $(VERSION) | sed 's/\./_/g'`
+
+release: upload webdocs
+
+upload:
+	! git-tag -l $(tag) | grep $(PACKAGE) > /dev/null
+	rm -f ChangeLog
+	$(MAKE) ChangeLog distcheck
+	git commit -m Generated. ChangeLog
+	git-tag -u b565716f! -m $(VERSION) $(tag)
+	git-push
+	git-push --tags
+	build-aux/gnupload --to ftp.gnu.org:$(PACKAGE) $(distdir).tar.gz
+	scp $(distdir).tar.gz $(distdir).tar.gz.sig igloo.linux.gr:~ftp/pub/gnutls/libtasn1/
+	ssh igloo.linux.gr 'cd ~ftp/pub/gnutls/libtasn1/ && sha1sum *.tar.gz > CHECKSUMS'
+	cp $(distdir).tar.gz $(distdir).tar.gz.sig ../releases/$(PACKAGE)/
+	make webdocs
+
+webdocs:
+	cd doc && ../build-aux/gendocs.sh -o ../$(htmldir)/manual $(PACKAGE) "$(PACKAGE_NAME)"
+	cp -v doc/reference/html/*.html doc/reference/html/*.png doc/reference/html/*.devhelp doc/reference/html/*.css $(htmldir)/reference/
+	cd $(htmldir) && \
+		cvs commit -m "Update." manual/ reference/
