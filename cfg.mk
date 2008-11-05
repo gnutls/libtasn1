d5fd2776f34683a62369b700b0f10720e09e98e2
Simon Josefsson <simon@josefsson.org>
2008-11-05 15:01:51 +0100

    Add texinfo stylesheet.

--- cfg.mk
+++ cfg.mk
@@ -66,7 +66,8 @@ upload:
 	cp $(distdir).tar.gz $(distdir).tar.gz.sig ../releases/gnutls/$(PACKAGE)/
 
 webdocs:
-	cd doc && ../build-aux/gendocs.sh -o ../$(htmldir)/manual $(PACKAGE) "$(PACKAGE_NAME)"
+	cd doc && ../build-aux/gendocs.sh --html "--css-include=texinfo.css" \
+		-o ../$(htmldir)/manual/ $(PACKAGE) "$(PACKAGE_NAME)"
 	cp -v doc/reference/html/*.html doc/reference/html/*.png doc/reference/html/*.devhelp doc/reference/html/*.css $(htmldir)/reference/
 
 upload-web:
