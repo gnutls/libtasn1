0b1cce8661cbdbb17d2c53ea314d442567bc508d
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-03-14 15:27:21 +0100

    corrected delete_unneeded_choice_fields().

--- lib/decoding.c
+++ lib/decoding.c
@@ -813,9 +813,12 @@ _asn1_get_indefinite_length_string (const unsigned char *der, int *len)
 
 static void delete_unneeded_choice_fields(asn1_node p)
 {
-  if (p->right)
+  asn1_node p2;
+
+  while (p->right)
     {
-      asn1_delete_structure (&p->right);
+      p2 = p->right;
+      asn1_delete_structure (&p2);
     }
 }
 
