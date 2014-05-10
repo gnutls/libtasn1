94c8a2d9f018c22116f5c3fef8b81504a1ab471b
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-05-10 13:53:01 +0200

    cleaned up a bit _asn1_remove_node().

--- lib/parser_aux.c
+++ lib/parser_aux.c
@@ -458,12 +458,16 @@ _asn1_remove_node (asn1_node node, unsigned int flags)
   if (node == NULL)
     return;
 
-  if (flags & ASN1_DELETE_FLAG_ZEROIZE && node->value) {
-    safe_memset(node->value, 0, node->value_len);
-  }
+  if (node->value != NULL)
+    {
+      if (flags & ASN1_DELETE_FLAG_ZEROIZE)
+        {
+          safe_memset(node->value, 0, node->value_len);
+        }
 
-  if (node->value != NULL && node->value != node->small_value)
-    free (node->value);
+      if (node->value != node->small_value)
+        free (node->value);
+    }
   free (node);
 }
 
