9bed7ad7a65e7a6b1d879ef8f2b5bd8ef244a04d
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-04-04 14:46:58 +0200

    Simplify _asn1_append_value() and avoid memcpy's with zero length
    Based on patch of Pascal Cuoq <pascal.cuoq@trust-in-soft.com>

--- lib/parser_aux.c
+++ lib/parser_aux.c
@@ -316,22 +316,14 @@ _asn1_append_value (asn1_node node, const void *value, unsigned int len)
 {
   if (node == NULL)
     return node;
-  if (node->value != NULL && node->value != node->small_value)
-    {
-      /* value is allocated */
-      int prev_len = node->value_len;
-      node->value_len += len;
-      node->value = _asn1_realloc (node->value, node->value_len);
-      if (node->value == NULL)
-	{
-	  node->value_len = 0;
-	  return NULL;
-	}
-      memcpy (&node->value[prev_len], value, len);
 
-      return node;
-    }
-  else if (node->value == node->small_value)
+  if (node->value == NULL)
+    return _asn1_set_value (node, value, len);
+
+  if (len == 0)
+    return node;
+
+  if (node->value == node->small_value)
     {
       /* value is in node */
       int prev_len = node->value_len;
@@ -342,13 +334,31 @@ _asn1_append_value (asn1_node node, const void *value, unsigned int len)
 	  node->value_len = 0;
 	  return NULL;
 	}
-      memcpy (node->value, node->small_value, prev_len);
+
+      if (prev_len > 0)
+        memcpy (node->value, node->small_value, prev_len);
+
       memcpy (&node->value[prev_len], value, len);
 
       return node;
     }
-  else				/* node->value == NULL */
-    return _asn1_set_value (node, value, len);
+  else /* if (node->value != NULL && node->value != node->small_value) */
+    {
+      /* value is allocated */
+      int prev_len = node->value_len;
+      node->value_len += len;
+
+      node->value = _asn1_realloc (node->value, node->value_len);
+      if (node->value == NULL)
+	{
+	  node->value_len = 0;
+	  return NULL;
+	}
+
+      memcpy (&node->value[prev_len], value, len);
+
+      return node;
+    }
 }
 
 /******************************************************************/
