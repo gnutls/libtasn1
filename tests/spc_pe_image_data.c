903bcd40b162eec7dc78e7305871a658e38a3de3
Andreas Schneider <asn@samba.org>
2016-10-24 11:16:56 +0200

    Add spc_pe_image_data test
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

---
+++ tests/spc_pe_image_data.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libtasn1.h>
+
+int main (int argc, char** argv)
+{
+	int result = 0, len;
+	asn1_node definitions = NULL, node1 = NULL;
+	char errorDescription[ASN1_MAX_ERROR_DESCRIPTION_SIZE];
+	const char *choicefile = getenv ("ASN1_MSCAT");
+	const char *datafile = getenv ("ASN1_SPC_PE_IMAGE_DATA");
+	FILE *fp;
+	char data[1024];
+	int data_size = sizeof (data);
+	unsigned etype;
+	int cmp;
+
+	if (choicefile == NULL) {
+		choicefile = "mscat.asn";
+	}
+
+	if (datafile == NULL) {
+		datafile = "spc_pe_image_data.der";
+	}
+
+	result = asn1_parser2tree(choicefile, &definitions, errorDescription);
+	if (result != ASN1_SUCCESS) {
+		fprintf(stderr, "error in %d: %s\n", __LINE__, errorDescription);
+		exit (1);
+	}
+
+	result = asn1_create_element(definitions,
+				     "CATALOG.SpcPEImageData",
+				     &node1);
+	if (result != ASN1_SUCCESS) {
+		fprintf(stderr, "error in %d\n", __LINE__);
+		exit (1);
+	}
+
+	fp = fopen(datafile, "rb");
+	if (fp == NULL) {
+		fprintf (stderr, "error in %d\n", __LINE__);
+		exit(1);
+	}
+	data_size = fread(data, 1, sizeof(data), fp);
+	fclose(fp);
+
+	result = asn1_der_decoding2(&node1,
+				    data,
+				    &data_size,
+				    ASN1_DECODE_FLAG_STRICT_DER,
+				    errorDescription);
+	if (result != ASN1_SUCCESS) {
+		fprintf(stderr, "error in %d: decoding: %s\n", __LINE__, errorDescription);
+		exit(1);
+	}
+
+	len = 0;
+	result = asn1_read_value_type(node1,
+				      "flags",
+				      NULL,
+				      &len,
+				      &etype);
+	if (result != ASN1_MEM_ERROR) {
+		fprintf(stderr, "error in %d: %s\n", __LINE__, errorDescription);
+		exit(1);
+	}
+
+	if ((size_t)len >= sizeof(data)) {
+		fprintf(stderr,
+			"error len=%d, sizeof(data)=%zu\n",
+			len,
+			sizeof(data));
+		exit(1);
+	}
+
+	result = asn1_read_value(node1, "flags", data, &len);
+	if (result != ASN1_SUCCESS) {
+		fprintf(stderr, "error in %d: %s\n", __LINE__, errorDescription);
+		exit (1);
+	}
+
+	etype = 0;
+	len = 0;
+	result = asn1_read_value_type(node1, "link", NULL, &len, &etype);
+	if (result != ASN1_MEM_ERROR)
+	{
+		fprintf(stderr,
+			"error in %d: result is: %s\n",
+			__LINE__,
+			asn1_strerror(result));
+		exit(1);
+	}
+
+	if (etype != ASN1_ETYPE_CHOICE) {
+		fprintf(stderr,
+			"error in %d: The type (%u) doesn't match choice.\n",
+			__LINE__,
+			etype);
+		exit(1);
+	}
+
+	if (len != 5) {
+		fprintf (stderr, "length doesn't match the expected (got: %d, should be: 20)\n", len);
+		exit (1);
+	}
+
+	len = sizeof(data);
+	result = asn1_read_value(node1, "link", data, &len);
+	if (result != ASN1_SUCCESS) {
+		fprintf(stderr, "error in %d: %s\n", __LINE__, errorDescription);
+		exit (1);
+	}
+
+	cmp = memcmp(data, "file", len);
+	if (cmp != 0) {
+		fprintf(stderr, "contents don't match\n");
+		exit(1);
+	}
+
+	printf("SUCCESS\n");
+
+	asn1_delete_structure (&node1);
+	asn1_delete_structure (&definitions);
+
+	return 0;
+}
