d4dffef79128b18406d34e43fa0701e3b6489617
ihsinme <ihsinme@gmail.com>
2021-03-01 07:31:14 +0000

    I believe your checks are not correct. in my opinion they are equivalent to checks !=. I suggest a simple fix. only fix!

--- lib/gstr.c
+++ lib/gstr.c
@@ -40,7 +40,7 @@ _asn1_str_cat (char *dest, size_t dest_tot_size, const char *src)
     }
   else
     {
-      if (dest_tot_size - dest_size > 0)
+      if (dest_tot_size > dest_size)
 	{
 	  strncat (dest, src, (dest_tot_size - dest_size) - 1);
 	  dest[dest_tot_size - 1] = 0;
